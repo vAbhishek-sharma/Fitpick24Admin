a27e16ac7dfd209b68c8f8faf887a4dd47fd72f1
vAbhishek-sharma <abhishek.b.sharma1994@gmail.com>
2022-06-21 16:13:51 +0530

    added devise to the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Trainer.first_or_create(email:'test1@avayath.com' ,
+    password:'password' ,
+    password_confirmation:'password',
+    role: Trainer.roles[:admin],
+)
